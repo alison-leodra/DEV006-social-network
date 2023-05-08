c5b47592295727a5f4e3f2d6905626efc549a7e2
Lizette Pérez <lizette.perezcorrea@gmail.com>
2023-05-08 13:02:05 -0400

    update

--- src/main.js
+++ src/main.js
@@ -7,4 +7,27 @@ const routes = [
   { path: '/', component: home },
   { path: '/register', component: register },
   { path: '/registerEmail', component: registerEmail },
+  { path: '/error', component: registerEmail },
 ];
+
+const defaultRoute = '/';
+const root = document.getElementById('id');
+
+const navigateTo = (hash) => {
+  const route = routes.find((routeFound) => routeFound.path === hash);
+  
+  if (route && route.component) {
+    window.history.pushState(
+      {},
+      route.path,
+      window.location.origin + route.path,
+    );
+
+    if (root.firstChild) {
+      root.removeChild(root.firstChild);
+    }
+    root.appendChild(route.component());
+   } else {
+    navigateTo('/error');
+  }
+}
