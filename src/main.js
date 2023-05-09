de8511f1666b08da78f621b682539c36e3031993
Lizette Pérez <lizette.perezcorrea@gmail.com>
2023-05-08 21:23:20 -0400

    update

--- src/main.js
+++ src/main.js
@@ -1,18 +1,21 @@
 // file main.js finished
 import home from './templates/home.js';
 import error from './templates/error.js';
+import register from './templates/register.js';
+import registerEmail from './templates/registerEmail.js';
 
 const routes = [
   { path: '/', component: home },
   { path: '/error', component: error },
+  { path: '/register', component: register },
+  { path: '/registerEmail', component: registerEmail },
 ];
 
 const defaultRoute = '/';
 const root = document.getElementById('root');
 
-function navigateTo(hash) {
+const navigateTo = (hash) => {
   const route = routes.find((routeFound) => routeFound.path === hash);
-  
   if (route && route.component) {
     window.history.pushState(
       {},
@@ -24,10 +27,10 @@ function navigateTo(hash) {
       root.removeChild(root.firstChild);
     }
     root.appendChild(route.component(navigateTo));
-   } else {
+  } else {
     navigateTo('/error');
   }
-}
+};
 
 window.onpopstate = () => {
   navigateTo(window.location.pathname);
--- src/templates/home.js
+++ src/templates/home.js
@@ -1,61 +1,43 @@
 const home = (navigateTo) => {
-  const inicioTemplate = document.createElement('div');
-  inicioTemplate.classList.add('inicioTemplate');
+  const template = `
+    <div class="inicioTemplate">
+      <header>
+        <div class="imgLogo">
+          <img src="./img/logo2.png" alt="logo">
+        </div>
+      </header>
 
-  const header = document.createElement('header');
-  const imgLogo = document.createElement('div');
-  const logo = document.createElement('img');
-  logo.src = './img/logo2.png';
-  imgLogo.classList.add('imgLogo');
-  imgLogo.appendChild(logo);
-  header.appendChild(imgLogo);
-  inicioTemplate.appendChild(header);
+      <main>
+        <div class="container">
+          <h2 class="tittle">Encuentra a tus compañeros de juego perfectos</h2>
+          <form id="logInForm">
+            <input type="text" id="logInEmail" class="formControl" placeholder="email@correo.com" required>
+            <input type="password" id="logInPassword" class="formControl" placeholder="contraseña" required>
+            <button type="button" class="logInbtn">Inciar Sesión</button> 
+            <button type="button" class="signbtn">Registrarse</button>
+          </form>
+        </div>
+      </main>
 
-  const main = document.createElement('main');
-  const container = document.createElement('div');
-  container.classList.add('container');
-  const title = document.createElement('h2');
-  title.classList.add('tittle');
-  title.textContent = 'Encuentra a tus compañeros de juego perfectos';
-  const form = document.createElement('form');
-  form.id = 'logInForm';
-  const emailInput = document.createElement('input');
-  emailInput.type = 'text';
-  emailInput.id = 'logInEmail';
-  emailInput.classList.add('formControl');
-  emailInput.placeholder = 'email@correo.com';
-  emailInput.required = true;
-  const passwordInput = document.createElement('input');
-  passwordInput.type = 'password';
-  passwordInput.id = 'logInPassword';
-  passwordInput.classList.add('formControl');
-  passwordInput.placeholder = 'contraseña';
-  passwordInput.required = true;
-  const loginBtn = document.createElement('button');
-  loginBtn.type = 'button';
-  loginBtn.classList.add('logInbtn');
-  loginBtn.textContent = 'Iniciar Sesión';
-  const signBtn = document.createElement('button');
-  signBtn.type = 'button';
-  signBtn.classList.add('signbtn');
-  signBtn.textContent = 'Registrarse';
-  signBtn.onclick = () => {
+      <footer>
+        <img src="./img/Sin título-4.png" alt="footer">
+      </footer>
+    </div>
+  `;
+
+  const element = document.createElement('div');
+  element.innerHTML = template.trim();
+
+  // Obtiene el botón "Registrarse" por su clase
+  const signBtn = element.querySelector('.signbtn');
+
+  // Agrega el evento "click" al botón "Registrarse"
+  signBtn.addEventListener('click', () => {
     navigateTo('/register');
-  };
-  form.append(emailInput, passwordInput, loginBtn, signBtn);
-  container.appendChild(title);
-  container.appendChild(form);
-  main.appendChild(container);
-  inicioTemplate.appendChild(main);
+  });
 
-  const footer = document.createElement('footer');
-  const footerImg = document.createElement('img');
-  footerImg.src = './img/Sin título-4.png';
-  footerImg.alt = 'footer';
-  footer.appendChild(footerImg);
-  inicioTemplate.appendChild(footer);
-
-  return inicioTemplate;
+  // Retorna el elemento del DOM creado a partir de la plantilla
+  return element.firstChild;
 };
 
 export default home;
