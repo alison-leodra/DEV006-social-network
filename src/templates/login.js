8eb2a38500b16c9c20bc21e586de5eefc6297508
yessicapv-24 <yessicapv_24@hotmail.com>
2023-05-17 15:19:56 -0600

    login nodo

--- src/templates/login.js
+++ src/templates/login.js
@@ -1,7 +1,7 @@
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { auth } from '../firebase.js';
-import error from './error.js';
+import { auth } from "../firebase.js";
+import error from "./error.js";
 
 const login = (navigateTo) => {
   const template = `
@@ -13,24 +13,24 @@ const login = (navigateTo) => {
       </div>
     </header>
 
-    <main>
-      <div class="container">
-        <form id="logInForm">
-          <input type="email" id="logInEmail" class="formControl" placeholder="email@correo.com" required>
-          <p class="emailError"></p>
-          <input type="password" id="logInPassword" class="formControl" placeholder="contraseña" required>
-          <p class="passwordError"></p>
-          <div>
-            <button type="button" class="logInbtn">Inciar con correo</button>
-            <button type="button" class="logInGoogle">Inciar con Google</button>
-            <p>
-              <a href="#" class="forgotBtn">¿Olvidaste tu contraseña?</a>
-            </p>
+    // <main>
+      // <div class="container">
+        // <form id="logInForm">
+        //   <input type="email" id="logInEmail" class="formControl" placeholder="email@correo.com" required>
+        //   <p class="emailError"></p>
+        //   <input type="password" id="logInPassword" class="formControl" placeholder="contraseña" required>
+        //   <p class="passwordError"></p>
+          // <div>
+          //   <button type="button" class="logInbtn">Inciar con correo</button>
+          //   <button type="button" class="logInGoogle">Inciar con Google</button>
+            // <p>
+            //   <a href="#" class="forgotBtn">¿Olvidaste tu contraseña?</a>
+            // </p>
             
-          </div>
-        </form>
-      </div>
-    </main>
+          // </div>
+        // </form>
+      // </div>
+    // </main>
 
     <footer>
       <img src="./img/Sin título-4.png" alt="footer">
@@ -39,86 +39,170 @@ const login = (navigateTo) => {
     </div>
   `;
 
+  const element = document.createElement("div");
+  const loginContainer = document.createElement("div");
+  loginContainer.classList.add("login");
 
-  const element = document.createElement('div');
-  const 
+  const templateHeader = ` <header>
+  <div class="imgLogo">
+    <img src="./img/logo2.png" alt="logo">
+    <h2 class="tittle">Encuentra a tus compañeros de juego perfectos</h2>
+  </div>
+</header> `;
+
+  loginContainer.innerHTML = templateHeader;
+  const main = document.createElement("main");
+
+  const container = document.createElement("div");
+  container.classList.add("container");
+
+  const form = document.createElement("form");
+  form.setAttribute('id', 'logInForm');
+
+  const inputEmail = document.createElement('input');
+  inputEmail.setAttribute('type', 'email');
+  inputEmail.setAttribute('id','logInEmail');
+  inputEmail.classList.add('formControl');
+  inputEmail.setAttribute('placeholder','email@correo.com');
+  inputEmail.setAttribute('required', '');
+
+  const paragraphEmail = document.createElement('p');
+  paragraphEmail.classList.add('emailError');
+
+  const inputPassword = document.createElement('input');
+  inputPassword.setAttribute('type', 'password');
+  inputPassword.setAttribute('id','logInPassword');
+  inputPassword.classList.add('formControl');
+  inputPassword.setAttribute('placeholder','contraseña');
+  inputPassword.setAttribute('required', '');
+  
+  const paragraphError = document.createElement('p');
+  paragraphError.classList.add('passwordError');
+  
+  const divLoginBtnContainer = document.createElement('div');
+  const logInBtn = document.createElement('button');
+  logInBtn.setAttribute('type', 'button');
+  logInBtn.classList.add('logInbtn');
+  logInBtn.innerText = 'Inciar con correo';
+
+  const loginGoogleBtn = document.createElement('button');
+  loginGoogleBtn.setAttribute('type', 'button');
+  loginGoogleBtn.classList.add('logInGoogle');
+  loginGoogleBtn.innerText = 'Inciar con Google';
+
+  const pContainerForgot = document.createElement('p');
+  const forgotBtn = document.createElement('a');
+  forgotBtn.setAttribute('href','#');
+  forgotBtn.classList.add('forgotBtn');
+  forgotBtn.innerText = '¿Olvidaste tu contraseña?';
+
+  const registerFooter = document.createElement('footer');
+  const  imgGeneral =document.createElement('img');
+  imgGeneral.setAttribute('src', './img/Sin título-4.png');
+  imgGeneral.setAttribute('alt', 'dos personas jugando videojuegos');
+  
+  const signUpButton = document.createElement('a');
+  signUpButton.setAttribute('href','');
+  signUpButton.classList.add('signUpButton');
+  signUpButton.classList.add('registerHome');
+  signUpButton.innerText = '¿Aún no tienes cuenta? Registrate aquí';
+  
+  pContainerForgot.append(forgotBtn);
+  divLoginBtnContainer.append(logInBtn);
+  divLoginBtnContainer.append(loginGoogleBtn);
+  divLoginBtnContainer.append(pContainerForgot);
+
+  form.append(inputEmail);
+  form.append(paragraphEmail);
+  form.append(inputPassword);
+  form.append(paragraphError);
+  form.append(divLoginBtnContainer);
+
+  container.append(form);
+  main.append(container);
+
+  registerFooter.append(imgGeneral);
+  registerFooter.append(signUpButton);
+
+  loginContainer.append(main);
+  loginContainer.append(registerFooter);
+  element.append(loginContainer);
+
 
 
   // Obtiene el botón "Registrarse" por su clase
-  const signUpButton = element.querySelector('.signUpButton');
+
 
   // Agrega el evento "click" al botón "Registrarse"
-  signUpButton.addEventListener('click', (e) => {
+  signUpButton.addEventListener("click", (e) => {
     e.preventDefault();
-    navigateTo('/register');
+    navigateTo("/register");
   });
 
-    const forgotBtn = element.querySelector('.forgotBtn');
-    forgotBtn.addEventListener('click', (e) => {
-      e.preventDefault();
-      navigateTo('/recovery');
-    });
+  
+   forgotBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    navigateTo("/recovery");
+  });
 
-  document.addEventListener('DOMContentLoaded', () => {
-    // Call the home function here
-    const logInbtn = document.querySelector(".logInbtn");
-    logInbtn.addEventListener('click', async (e) => {
+  
+     logInBtn.addEventListener("click", async (e) => {
       e.preventDefault();
-      const email = document.querySelector('#logInEmail').value;
-      const password = document.querySelector('#logInPassword').value;
-      const passwordError = document.querySelector('.passwordError');
-      const emailError = document.querySelector('.emailError');
+      const email = document.querySelector("#logInEmail").value;
+      const password = document.querySelector("#logInPassword").value;
+      const passwordError = document.querySelector(".passwordError");
+      const emailError = document.querySelector(".emailError");
 
       try {
-        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
         console.log(userCredential);
-        navigateTo('/home');
-
+        navigateTo("/home");
       } catch (error) {
         console.log(error.message);
         console.log(error.code);
 
-        if (error.code === 'auth/invalid-email') {
-          emailError.style.display = 'block';
-          passwordError.style.display = 'none';
-          emailError.textContent = 'El correo electrónico ingresado no es válido.';
-        } else if (error.code === 'auth/user-not-found') {
-          emailError.style.display = 'block';
-          passwordError.style.display = 'none';
-          emailError.textContent = 'El correo electrónico ingresado no ha sido encontrado';
-        } else if (error.code === 'auth/wrong-password') {
-          passwordError.style.display = 'block';
-          emailError.style.display = 'none';
-          passwordError.textContent = 'La contraseña es incorrecta';
-        } else if (error.code === 'auth/missing-password') {
-          passwordError.style.display = 'block';
-          emailError.style.display = 'none';
-          passwordError.textContent = 'Debe ingresar una contraseña.';
+        if (error.code === "auth/invalid-email") {
+          emailError.style.display = "block";
+          passwordError.style.display = "none";
+          emailError.textContent =
+            "El correo electrónico ingresado no es válido.";
+        } else if (error.code === "auth/user-not-found") {
+          emailError.style.display = "block";
+          passwordError.style.display = "none";
+          emailError.textContent =
+            "El correo electrónico ingresado no ha sido encontrado";
+        } else if (error.code === "auth/wrong-password") {
+          passwordError.style.display = "block";
+          emailError.style.display = "none";
+          passwordError.textContent = "La contraseña es incorrecta";
+        } else if (error.code === "auth/missing-password") {
+          passwordError.style.display = "block";
+          emailError.style.display = "none";
+          passwordError.textContent = "Debe ingresar una contraseña.";
         }
       }
-    })
-  });
-  const signInFormGoogle = element.querySelector('.logInGoogle');
+    });
+  ;
+  const signInFormGoogle = element.querySelector(".logInGoogle");
   // Agrega el evento "click" al botón "Registrarse"
-  signInFormGoogle.addEventListener('click', async (e) => {
+  signInFormGoogle.addEventListener("click", async (e) => {
     e.preventDefault();
     const provider = new GoogleAuthProvider();
     try {
       const credentials = await signInWithPopup(auth, provider);
       console.log(credentials);
-      navigateTo('/home');
-    }
-    catch (error) {
+      navigateTo("/home");
+    } catch (error) {
       console.log(error);
     }
   });
 
-
-
-
-
   // Retorna el elemento del DOM creado a partir de la plantilla
-  return element.firstChild;
+  return element;
 };
 
 export default login;
