e93e676b49c9e79c2e3874577d3cbfac4a30caec
alison-leodra <127059922+alison-leodra@users.noreply.github.com>
2023-05-18 11:25:44 -0400

    nada importante

--- src/templates/login.js
+++ src/templates/login.js
@@ -61,9 +61,9 @@ const login = (navigateTo) => {
 
   const inputEmail = document.createElement('input');
   inputEmail.setAttribute('type', 'email');
-  inputEmail.setAttribute('id','logInEmail');
+  inputEmail.setAttribute('id', 'logInEmail');
   inputEmail.classList.add('formControl');
-  inputEmail.setAttribute('placeholder','email@correo.com');
+  inputEmail.setAttribute('placeholder', 'email@correo.com');
   inputEmail.setAttribute('required', '');
 
   const paragraphEmail = document.createElement('p');
@@ -71,14 +71,14 @@ const login = (navigateTo) => {
 
   const inputPassword = document.createElement('input');
   inputPassword.setAttribute('type', 'password');
-  inputPassword.setAttribute('id','logInPassword');
+  inputPassword.setAttribute('id', 'logInPassword');
   inputPassword.classList.add('formControl');
-  inputPassword.setAttribute('placeholder','contraseña');
+  inputPassword.setAttribute('placeholder', 'contraseña');
   inputPassword.setAttribute('required', '');
-  
+
   const paragraphError = document.createElement('p');
   paragraphError.classList.add('passwordError');
-  
+
   const divLoginBtnContainer = document.createElement('div');
   const logInBtn = document.createElement('button');
   logInBtn.setAttribute('type', 'button');
@@ -92,21 +92,21 @@ const login = (navigateTo) => {
 
   const pContainerForgot = document.createElement('p');
   const forgotBtn = document.createElement('a');
-  forgotBtn.setAttribute('href','#');
+  forgotBtn.setAttribute('href', '#');
   forgotBtn.classList.add('forgotBtn');
   forgotBtn.innerText = '¿Olvidaste tu contraseña?';
 
   const registerFooter = document.createElement('footer');
-  const  imgGeneral =document.createElement('img');
+  const imgGeneral = document.createElement('img');
   imgGeneral.setAttribute('src', './img/Sin título-4.png');
   imgGeneral.setAttribute('alt', 'dos personas jugando videojuegos');
-  
+
   const signUpButton = document.createElement('a');
-  signUpButton.setAttribute('href','');
+  signUpButton.setAttribute('href', '');
   signUpButton.classList.add('signUpButton');
   signUpButton.classList.add('registerHome');
   signUpButton.innerText = '¿Aún no tienes cuenta? Registrate aquí';
-  
+
   pContainerForgot.append(forgotBtn);
   divLoginBtnContainer.append(logInBtn);
   divLoginBtnContainer.append(loginGoogleBtn);
@@ -139,53 +139,53 @@ const login = (navigateTo) => {
     navigateTo("/register");
   });
 
-  
-   forgotBtn.addEventListener("click", (e) => {
+
+  forgotBtn.addEventListener("click", (e) => {
     e.preventDefault();
     navigateTo("/recovery");
   });
 
-  
-     logInBtn.addEventListener("click", async (e) => {
-      e.preventDefault();
-      const email = document.querySelector("#logInEmail").value;
-      const password = document.querySelector("#logInPassword").value;
-      const passwordError = document.querySelector(".passwordError");
-      const emailError = document.querySelector(".emailError");
 
-      try {
-        const userCredential = await signInWithEmailAndPassword(
-          auth,
-          email,
-          password
-        );
-        console.log(userCredential);
-        navigateTo("/home");
-      } catch (error) {
-        console.log(error.message);
-        console.log(error.code);
+  logInBtn.addEventListener("click", async (e) => {
+    e.preventDefault();
+    const email = document.querySelector("#logInEmail").value;
+    const password = document.querySelector("#logInPassword").value;
+    const passwordError = document.querySelector(".passwordError");
+    const emailError = document.querySelector(".emailError");
 
-        if (error.code === "auth/invalid-email") {
-          emailError.style.display = "block";
-          passwordError.style.display = "none";
-          emailError.textContent =
-            "El correo electrónico ingresado no es válido.";
-        } else if (error.code === "auth/user-not-found") {
-          emailError.style.display = "block";
-          passwordError.style.display = "none";
-          emailError.textContent =
-            "El correo electrónico ingresado no ha sido encontrado";
-        } else if (error.code === "auth/wrong-password") {
-          passwordError.style.display = "block";
-          emailError.style.display = "none";
-          passwordError.textContent = "La contraseña es incorrecta";
-        } else if (error.code === "auth/missing-password") {
-          passwordError.style.display = "block";
-          emailError.style.display = "none";
-          passwordError.textContent = "Debe ingresar una contraseña.";
-        }
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(userCredential);
+      navigateTo("/home");
+    } catch (error) {
+      console.log(error.message);
+      console.log(error.code);
+
+      if (error.code === "auth/invalid-email") {
+        emailError.style.display = "block";
+        passwordError.style.display = "none";
+        emailError.textContent =
+          "El correo electrónico ingresado no es válido.";
+      } else if (error.code === "auth/user-not-found") {
+        emailError.style.display = "block";
+        passwordError.style.display = "none";
+        emailError.textContent =
+          "El correo electrónico ingresado no ha sido encontrado";
+      } else if (error.code === "auth/wrong-password") {
+        passwordError.style.display = "block";
+        emailError.style.display = "none";
+        passwordError.textContent = "La contraseña es incorrecta";
+      } else if (error.code === "auth/missing-password") {
+        passwordError.style.display = "block";
+        emailError.style.display = "none";
+        passwordError.textContent = "Debe ingresar una contraseña.";
       }
-    });
+    }
+  });
   ;
   const signInFormGoogle = element.querySelector(".logInGoogle");
   // Agrega el evento "click" al botón "Registrarse"
